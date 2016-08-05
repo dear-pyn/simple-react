85778e8bcf5dcd08ded05481989d2edb3fa3ddf5
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2016-08-05 15:33:20 +0700

    Adjust style for storybook

--- src/components/stories/Counter.js
+++ src/components/stories/Counter.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 import Counter from '../Counter.js'
+import '../../App.scss'
 
 storiesOf('Counter', module)
   .add('simple Counter', () => (
-    <Counter onClick={action('clicked')} clickCounter={999} />
+    <div className='container'>
+      <Counter onClick={action('clicked')} counterValue={999}  />
+    </div>
   ));
